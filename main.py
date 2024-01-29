ce7375aadc13c16ea9b103688291aeae23dfd301
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2024-01-29 13:03:35 +0000

    implemented computer vs player and player vs player

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-board = ["","","", "", "", "", "", "", ""]
-player1 = "O"
-player2 = "X"
+import random
 
+board = ["","","", "", "", "", "", "", ""]
+player1_symbol = "O"
+player2_symbol = "X"
+computer_symbol = "C"
 
 def draw_board(board):
     for i in range(0, 9, 3):
@@ -37,8 +39,15 @@ def check_game_winner(board,player):
     ):
        return True  
     return False
+def computer_fill_board(board):
+    position = random.randrange(0,9)
+    while (board[position] != ""):
+        position = random.randrange(0,9)
+         
+    board[position] = "C"
+    draw_board(board)
 
-def fill_board(board,player):
+def player_fill_board(board,player):
     position = int(input("Please choose a position from 0-8:"))
     
     while (board[position] != ""):
@@ -46,22 +55,40 @@ def fill_board(board,player):
         position = int(input("Please choose a position from 0-9:"))
          
     board[position] = player
-    draw_board(board) 
+    draw_board(board)
+
+def game_play(board,symbol,symbol2):
+    while True:
+        if symbol == "C":
+            print("Computer playing")
+            
+            computer_fill_board(board)
+            if check_game_winner(board,symbol):
+                print("Computer wins")
+                break
+        if symbol == "X" or symbol2 == "X":    
+            print("Player One playing")
+
+            player_fill_board(board,symbol)
+            if check_game_winner(board,symbol):
+                print("Player One wins")
+                break
+        else:
+            print("Player Two playing")
+            player_fill_board(board,symbol2)
+            if check_game_winner(board,symbol2):
+                print("Player Two wins")
+                break
+    draw_board(board)
 
 def main():
-    while True:
-        print("Player One playing")
+    game_mode =int(input("Please choose 1 for Computer vs Player or 2 for Player vs Player: "))
+    if game_mode == 1:
+        game_play(board,computer_symbol,player1_symbol)
+    elif game_mode == 2:
+        game_play(board,player1_symbol,player2_symbol)
+    else:
+        print("Please choose the correct option next time")
         
-        fill_board(board,player1)
-        if check_game_winner(board,player1):
-            print("Player One wins")
-            break
-        
-        print("Player Two playing")
-        fill_board(board,player2)
-        if check_game_winner(board,player2):
-            print("Player Two wins")
-            break
-        
-# if __name__ == "main":
-main()
+if __name__ == "__main__":
+    main()
