688edfe3cd5ff7b12481b2cdb7c4ed24bb9fb424
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2024-01-29 13:50:59 +0000

    minor refactorings

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import random
 
-board = ["","","", "", "", "", "", "", ""]
+board = ["", "", "", "", "", "", "", "", ""]
 player1_symbol = "O"
 player2_symbol = "X"
 computer_symbol = "C"
+
 
 def draw_board(board):
     for i in range(0, 9, 3):
         print(board[i : i + 3])
 
 
-def check_game_winner(board,player):
+def check_game_winner(board, player):
     if (
         board[0]
         and board[1]
@@ -37,58 +38,69 @@ def check_game_winner(board,player):
         and board[4]
         and board[6] == player
     ):
-       return True  
+        return True
     return False
+
+
 def computer_fill_board(board):
-    position = random.randrange(0,9)
-    while (board[position] != ""):
-        position = random.randrange(0,9)
-         
+    position = random.randrange(0, 9)
+    while board[position] != "":
+        position = random.randrange(0, 9)
+
     board[position] = "C"
     draw_board(board)
 
-def player_fill_board(board,player):
+
+def player_fill_board(board, player):
     position = int(input("Please choose a position from 0-8:"))
-    
-    while (board[position] != ""):
-        print(f"Position {position} already occupied please choose a different position")
+
+    while board[position] != "":
+        print(
+            f"Position {position} already occupied please choose a different position"
+        )
         position = int(input("Please choose a position from 0-9:"))
-         
+
     board[position] = player
     draw_board(board)
 
-def game_play(board,symbol,symbol2):
+
+def game_play(board, symbol, symbol2):
     while True:
-        if symbol == "C":
+        if symbol == computer_symbol:
             print("Computer playing")
-            
+
             computer_fill_board(board)
-            if check_game_winner(board,symbol):
+            if check_game_winner(board, symbol):
                 print("Computer wins")
                 break
-        if symbol == "X" or symbol2 == "X":    
+        if symbol == player1_symbol or symbol2 == player1_symbol:
             print("Player One playing")
 
-            player_fill_board(board,symbol)
-            if check_game_winner(board,symbol):
+            player_fill_board(board, symbol)
+            if check_game_winner(board, symbol):
                 print("Player One wins")
                 break
         else:
             print("Player Two playing")
-            player_fill_board(board,symbol2)
-            if check_game_winner(board,symbol2):
+            player_fill_board(board, symbol2)
+            if check_game_winner(board, symbol2):
                 print("Player Two wins")
                 break
     draw_board(board)
 
+
 def main():
-    game_mode =int(input("Please choose 1 for Computer vs Player or 2 for Player vs Player: "))
+    game_mode = int(
+        input("Please choose 1 for Computer vs Player or 2 for Player vs Player: ")
+    )
     if game_mode == 1:
-        game_play(board,computer_symbol,player1_symbol)
+        game_play(board, computer_symbol, player1_symbol)
     elif game_mode == 2:
-        game_play(board,player1_symbol,player2_symbol)
+        game_play(board, player1_symbol, player2_symbol)
     else:
         print("Please choose the correct option next time")
-        
+
+
 if __name__ == "__main__":
     main()
+ 
